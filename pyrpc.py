d6d1d42b311eb7ab1fae2781cdb891b56adfc6c6
lzy2002 <liziyi0914@126.com>
2018-11-24 22:30:30 +0800

    finish server's JSON

--- pyrpc.py
+++ pyrpc.py
@@ -1,6 +1,8 @@
+import json
+import random
 
 
-class RPC():
+class RPCServer():
     def __init__(self):
         self.regf = {}
         self.regf_cfg = {}
@@ -12,77 +14,64 @@ class RPC():
 
     def getRegList(self):
         return {
-                'Variables': {k:v for k,v in self.regv_cfg.items()},
-                'Functions': {k:v for k,v in self.regf_cfg.items()},
-                'Classes': {k:v for k,v in self.regc_cfg.items()}
-                }
+            'Variables': {k: v
+                          for k, v in self.regv_cfg.items()},
+            'Functions': {k: v
+                          for k, v in self.regf_cfg.items()},
+            'Classes': {k: v
+                        for k, v in self.regc_cfg.items()}
+        }
 
-    def regVariable(self,name,typestr,value=None):
+    def regVariable(self, name, typestr, value=None):
         self.regv[name] = value
-        self.regv_cfg[name] = {
-                'Name': name,
-                'Type': typestr
-                }
+        self.regv_cfg[name] = {'Name': name, 'Type': typestr}
 
-    def getVariable(self,name):
+    def getVariable(self, name):
         return self.regv[name]
 
-    def setVariable(self,name,value):
+    def setVariable(self, name, value):
         self.regv[name] = value
 
-    def regFunction(self,name,function,args={},ret='None'):
+    def regFunction(self, name, function, args={}, ret='None'):
         self.regf[name] = function
-        self.regf_cfg[name] = {
-                'Name': name,
-                'Args': args,
-                'Ret': ret
-                }
+        self.regf_cfg[name] = {'Name': name, 'Args': args, 'Ret': ret}
 
-    def callFunction(self,name,kwargs):
+    def callFunction(self, name, kwargs):
         return self.regf[name](**kwargs)
 
-    def regClass(self,name,cls,functions=[]):
+    def regClass(self, name, cls, functions=[]):
         self.regc[name] = cls
         self.regc_cfg[name] = {
-                'Name': name,
-                'Functions': [{
-                    'Name': f['name'],
-                    'Args': f['args'],
-                    'Ret': f['ret']
-                    } for f in functions]
-                }
+            'Name':
+            name,
+            'Functions': [{
+                'Name': f['name'],
+                'Args': f['args'],
+                'Ret': f['ret']
+            } for f in functions]
+        }
 
-    def createClassInstance(self,name,kwargs={}):
-        instName = '@'+name
+    def createClassInstance(self, name, kwargs={}):
+        r = random.Random()
+        instName = hex(r.randint(0, 256 * 256 * 256 * 256)) + '@' + name
+        while (instName in self.cInst):
+            instName = hex(r.randint(0, 256 * 256 * 256 * 256)) + '@' + name
         self.cInst[instName] = self.regc[name](**kwargs)
         return instName
 
-    def callClassFunction(self,instName,name,kwargs={}):
-        return getattr(self.cInst[instName],name)(**kwargs)
+    def callClassFunction(self, instName, name, kwargs={}):
+        return getattr(self.cInst[instName], name)(**kwargs)
 
-
-def add(a,b):
-    print(a+b)
-
-class Math():
-    def __init__(self):
-        print('Math is initing...')
-
-    def multiply(self,a,b):
-        print(a*b)
-
-t = 1024
-
-add(1,2)
-rpc = RPC()
-rpc.regVariable('t','Int',t)
-print(rpc.getVariable('t'))
-rpc.setVariable('t',512)
-print(rpc.getVariable('t'))
-rpc.regFunction('add',add,{'a':'Int','b':'Int'},'Int')
-rpc.callFunction('add',{'a':2,'b':3})
-rpc.regClass('Math',Math,[{'name':'multiply','args':[],'ret':''}])
-m = rpc.createClassInstance('Math')
-print(m)
-print(rpc.callClassFunction(m,'multiply',{'a':3,'b':4}))
-print(rpc.getRegList())
+    def executeJSON(self, jsonstr):
+        jsonobj = json.loads(jsonstr)
+        f = jsonobj['Function']
+        args = jsonobj['Args']
+        ret = {"Function": f, "Return": ''}
+        if '@' in f:
+            ret['Return'] = self.callClassFunction(f[:f.find('.')],
+                                                   f[f.find('.') + 1:], args)
+        elif '#' in f:
+            ret['Return'] = self.createClassInstance(f[f.find('#') + 1:], args)
+        else:
+            ret['Return'] = self.callFunction(f, args)
+        return ret
