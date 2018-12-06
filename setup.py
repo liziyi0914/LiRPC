97d4e5a2d13cb6be05d91dd7d1f9dbb686cbfb18
lzy2002 <liziyi0914@126.com>
2018-12-07 02:19:22 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
         name= 'LiRPC',
-        version= '0.1.1',
+        version= '0.1.2',
         description=('liziyi0914\'s RPC module'),
         author= 'liziyi0914',
         author_email= 'liziyi0914@gmail.com',
         license= 'GPL License',
-        packages= find_packages(),
+        package_dir= {'LiRPC':''},
+        packages= ['LiRPC'],
         platforms= ["all"],
         url= 'https://github.com/liziyi0914/LiRPC')
